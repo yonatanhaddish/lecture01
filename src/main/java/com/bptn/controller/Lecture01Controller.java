6f8efde155b929c328ba70c2833f6fedfb5a52bf
Yonatan Haddish <yonihailshn@gmail.com>
2022-11-01 22:48:21 -0400

    some practice on Loggers

---
+++ src/main/java/com/bptn/controller/Lecture01Controller.java
@@ -0,0 +1,30 @@
+package com.bptn.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Lecture01Controller {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@GetMapping("/")
+	public ResponseEntity<String> ping() {
+		
+		logger.debug("Executing ping()");
+		
+		return new ResponseEntity<String>("lecture01 running and running", HttpStatus.OK);
+	}
+
+}
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
