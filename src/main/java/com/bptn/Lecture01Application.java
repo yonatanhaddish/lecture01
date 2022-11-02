7d1311e3f96395e0ddf96e69fcff42c7ba467a44
Yonatan Haddish <yonihailshn@gmail.com>
2022-11-01 20:19:13 -0400

    initial setup

---
+++ src/main/java/com/bptn/Lecture01Application.java
@@ -0,0 +1,13 @@
+package com.bptn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bptn/Lecture01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bptn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
