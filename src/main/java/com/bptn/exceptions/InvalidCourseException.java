21454d434f38efdb67410d0d4d4d87e511e5ef50
Yonatan Haddish <yonihailshn@gmail.com>
2022-11-08 20:32:21 -0500

    exceptions handler package created

---
+++ src/main/java/com/bptn/exceptions/InvalidCourseException.java
@@ -0,0 +1,27 @@
+package com.bptn.exceptions;
+
+public class InvalidCourseException extends Exception {
+
+	public InvalidCourseException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidCourseException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidCourseException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidCourseException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
---
+++ src/main/java/com/bptn/exceptions/InvalidStudentIdException.java
@@ -0,0 +1,27 @@
+package com.bptn.exceptions;
+
+public class InvalidStudentIdException extends Exception {
+
+	public InvalidStudentIdException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidStudentIdException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidStudentIdException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidStudentIdException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
